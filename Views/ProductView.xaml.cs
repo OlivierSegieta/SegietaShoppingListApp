253282029ba273f32ca84cbd884bc3329d4b34c1
OlivierSegieta <oliseg25@gmail.com>
2025-01-14 14:19:03 +0100

    Final version

---
+++ Views/ProductView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SegietaShoppingListApp.Views
+{
+    public partial class ProductView : ContentView
+    {
+        public ProductView()
+        {
+            InitializeComponent();
+        }
+    }
+}
